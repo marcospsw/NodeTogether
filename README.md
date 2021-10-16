8a9097b253924b635b911a0ecb871316c688cde2
marcospsw <marcospsw96@gmail.com>
2021-10-16 09:46:54 -0300

    feat: readme e config front

---
+++ README.md
@@ -0,0 +1,9 @@
+##Andar a milha extra
+
+[ ] Envio de e-mail pros elogios
+
+[ ] Colocar em produção
+
+[ ] Implementar estrutura Clean Code
+
+[ ] Migrar para PostgresSQL
--- src/server.ts
+++ src/server.ts
@@ -3,32 +3,27 @@ import 'reflect-metadata';
 import './database';
 import 'express-async-errors';
 import router from './routes';
+import cors from 'cors';
 import ErrorResponse from './utils/ErrorResponse';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
-app.use(
-  (
-    err: ErrorResponse,
-    request: Request,
-    response: Response,
-    next: NextFunction
-  ) => {
-    if (err instanceof ErrorResponse) {
-      return response.status(err.statusCode).json({
-        statusCode: err.statusCode,
-        error: err.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: 500,
-      message: 'Internal Server Error',
+app.use((err: ErrorResponse, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof ErrorResponse) {
+    return response.status(err.statusCode).json({
+      statusCode: err.statusCode,
+      error: err.message,
     });
   }
-);
+
+  return response.status(500).json({
+    status: 500,
+    message: 'Internal Server Error',
+  });
+});
 
 app.listen(3000, () => console.log(`Server is online on port: ${3000}`));
